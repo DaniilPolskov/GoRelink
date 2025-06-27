0e3d80812d6a976aa81496301ba0ba8054b296dd
DaniilPolskov <saxap.super6@gmail.com>
2025-06-27 16:21:29 +0300

    Refactor: extract ID generation and storage logic into separate packages

---
+++ storage/storage.go
@@ -0,0 +1,27 @@
+package storage
+
+import "sync"
+
+type MemoryStore struct {
+	data map[string]string
+	mu   sync.RWMutex
+}
+
+func NewMemoryStore() *MemoryStore {
+	return &MemoryStore{
+		data: make(map[string]string),
+	}
+}
+
+func (s *MemoryStore) Save(id, url string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.data[id] = url
+}
+
+func (s *MemoryStore) Get(id string) (string, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	url, ok := s.data[id]
+	return url, ok
+}
